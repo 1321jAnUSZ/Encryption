55996f43bf71c4e0da8515baf7e30ae81bc16af8
1321jAnUSZ <janusz.ak.1321@gmail.com>
2022-11-29 20:17:27 +0100

    Dodaj pliki projektów.

---
+++ Encryption/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Encryption
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.KluczTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.KluczTextBox);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.DeleteButton);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(-3, -3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(802, 458);
+            this.panel1.TabIndex = 0;
+            // 
+            // KluczTextBox
+            // 
+            this.KluczTextBox.Location = new System.Drawing.Point(458, 33);
+            this.KluczTextBox.Name = "KluczTextBox";
+            this.KluczTextBox.Size = new System.Drawing.Size(91, 27);
+            this.KluczTextBox.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(80, 180);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(256, 38);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "ODSZYFROWANIE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(80, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(214, 38);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "SZYFROWANIE";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(315, 235);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(77, 55);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(416, 250);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(319, 27);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(54, 250);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(240, 27);
+            this.textBox4.TabIndex = 5;
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Image = ((System.Drawing.Image)(resources.GetObject("DeleteButton.Image")));
+            this.DeleteButton.Location = new System.Drawing.Point(655, 325);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(104, 62);
+            this.DeleteButton.TabIndex = 4;
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(315, 66);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(77, 55);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(416, 81);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(319, 27);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(54, 81);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(240, 27);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(334, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 21);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Podaj klucz:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox KluczTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Encryption/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Encryption
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.BackColor = Color.FromArgb(0, Color.Gray);
+            pictureBox2.BackColor = Color.FromArgb(0, Color.Gray);
+            label1.BackColor = Color.FromArgb(0, Color.Gray);
+            label2.BackColor = Color.FromArgb(0, Color.Gray);
+            label3.BackColor = Color.FromArgb(0, Color.Gray);
+            DeleteButton.BackColor = Color.FromArgb(0, Color.Gray);
+        }
+
+        string Szyfrowanie(string text, int klucz)
+        #region Szyfrowanie uniwersalne
+        {
+            string koniec = ""; //wyniczek
+            for(int i = 0; i<text.Length;i++)
+            {
+                if (text[i] != ' ' && text[i] != ';' && text[i] != '.')
+                {
+                    if (text[i] >= 'A' && text[i] <= 'Z')
+                    {
+                        koniec = koniec + (char)(65 + (text[i] - (65 - klucz)) % 26);
+                    }
+                    if (text[i] >= 'a' && text[i] <= 'z')
+                    {
+                        koniec = koniec + (char)(97 + (text[i] - (97 - klucz)) % 26);
+                    }
+                }
+                else koniec += text[i];
+            }
+            return koniec;
+        }
+        #endregion
+        string Odszyfrowanie(string text, int klucz)
+        {
+            string koniec = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] != ' ' && text[i] != ';' && text[i] != '.')
+                {
+                    if (text[i] >= 'A' && text[i] <= 'Z')
+                    {
+                        koniec = koniec + (char)(65 + (text[i] + (65 - klucz)) % 26);
+                    }
+                    if (text[i] >= 'a' && text[i] <= 'z')
+                    {
+                        koniec = koniec + (char)(97 + (text[i] + (85 - klucz)) % 26);
+                    }
+                }
+                else koniec += text[i];
+            }
+            return koniec;
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (KluczTextBox.Text == "0" && KluczTextBox.Text == " ")
+            {
+                MessageBox.Show("KLUCZ NIE MOŻE BYĆ ZEROWY! WCIŚNIJ USUŃ NA EKRANIE PRZED KOTYNUŁOWANIEM OPERACJI!");
+                pictureBox1.Enabled = false;
+                pictureBox2.Enabled = false;
+            }
+            else
+                try
+                {
+                    textBox2.Text += Szyfrowanie(textBox1.Text, Convert.ToInt32(KluczTextBox.Text));
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Podano złe parametry");
+                }
+            pictureBox1.Enabled = false;
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            KluczTextBox.Clear();
+            pictureBox1.Enabled = true;
+            pictureBox2.Enabled = true;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            if (KluczTextBox.Text == "0" && KluczTextBox.Text == " ")
+            {
+                MessageBox.Show("KLUCZ NIE MOŻE BYĆ ZEROWY! WCIŚNIJ USUŃ NA EKRANIE PRZED KOTYNUŁOWANIEM OPERACJI!");
+                pictureBox1.Enabled = false;
+                pictureBox2.Enabled = false;
+            }
+            else
+                try 
+                {
+                    textBox4.Text += Odszyfrowanie(textBox3.Text, Convert.ToInt32(KluczTextBox.Text));
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Podano złe parametry");
+                }
+            pictureBox2.Enabled = false;
+        }
+    }
+}
